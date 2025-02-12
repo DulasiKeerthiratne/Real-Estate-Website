3bdf1035e4fc693f2b07fc246277978ecb65a4bb
Dulasi Keerthiratne <dulasikeerthiratne@gmail.com>
2025-02-13 02:36:42 +0530

    refactor: change file name from FilterForm to Form

--- src/components/Form.js
+++ src/components/Form.js
@@ -1,9 +1,11 @@
 import React from 'react';
 import { Select, MenuItem, FormControl, InputLabel, TextField } from '@mui/material';
 
-const FilterForm = ({ typeFilter, setTypeFilter, minRooms, setMinRooms, maxRooms, setMaxRooms, minDate, setMinDate, maxDate, setMaxDate, postcode, setPostcode, propertyTypes }) => {
+// Search Form component
+const Form = ({ typeFilter, setTypeFilter, minRooms, setMinRooms, maxRooms, setMaxRooms, minDate, setMinDate, maxDate, setMaxDate, postcode, setPostcode, propertyTypes }) => {
   return (
     <form className="d-grid gap-3">
+      {/* Property Type Filter */}
       <FormControl fullWidth>
         <InputLabel>Property Type</InputLabel>
         <Select value={typeFilter} onChange={(e) => setTypeFilter(e.target.value)} label="Property Type">
@@ -13,13 +15,23 @@ const FilterForm = ({ typeFilter, setTypeFilter, minRooms, setMinRooms, maxRooms
           ))}
         </Select>
       </FormControl>
+      
+      {/* Min Rooms Filter */}
       <TextField id="minRooms" label="Min Rooms" type="number" value={minRooms} onChange={(e) => setMinRooms(e.target.value)} />
+      
+      {/* Max Rooms Filter */}
       <TextField id="maxRooms" label="Max Rooms" type="number" value={maxRooms} onChange={(e) => setMaxRooms(e.target.value)} />
+      
+      {/* Min Date Filter */}
       <TextField id="minDate" label="Min Date" type="date" value={minDate} onChange={(e) => setMinDate(e.target.value)} InputLabelProps={{ shrink: true }} />
+      
+      {/* Max Date Filter */}
       <TextField id="maxDate" label="Max Date" type="date" value={maxDate} onChange={(e) => setMaxDate(e.target.value)} InputLabelProps={{ shrink: true }} />
+      
+      {/* Postcode Filter */}
       <TextField id="postcode" label="Postcode" type="text" value={postcode} onChange={(e) => setPostcode(e.target.value)} />
     </form>
   );
 };
 
-export default FilterForm;
+export default Form;
